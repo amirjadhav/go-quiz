25eb466f233b6b4761262e1ce9560b119f11acea
amir jadhav <jadhav.amir100@gmial.com>
2023-07-05 20:47:20 +0530

    enabled cors

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 
 	app.Get("/", controller.HelloWorld)
 
